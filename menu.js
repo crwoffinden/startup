331bb9574326c5a77326c557c4ef0ede16f8c8a5
crwoffinden <crwoffinden@gmail.com>
2023-03-10 15:09:51 -0700

    modified:   menu.html 	new file:   menu.js

---
+++ menu.js
@@ -0,0 +1,15 @@
+function toProfile() {
+    window.location.href = "menu.html";
+}
+
+function newProject() {
+    window.location.href = "newProject.html";
+}
+
+function myProjects() {
+    window.location.href = "myProjects.html";
+}
+
+function whatsHot() {
+    window.location.href = "whatsHot.html";
+}
