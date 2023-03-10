1fb5bba52ec6fc0ddbbdba9cc46dafdf83418cf0
crwoffinden <crwoffinden@gmail.com>
2023-03-10 16:40:29 -0700

    modified:   profile.js

--- profile.js
+++ profile.js
@@ -1,9 +1,11 @@
 function fill() {
     const favorite = document.getElementById("favorite");
-    favorite.innerHTML = "<i class=\"bi bi-heart-fill\" id=\"favorite\" onclick=\"empty()\"></i>";
+    favorite.className = "bi bi-heart-fill"; 
+    favorite.onclick = "empty()";
 }
 
 function empty() {
     const favorite = document.getElementById("favorite");
-    favorite.innerHTML = "<i class=\"bi bi-heart\" id=\"favorite\" onclick=\"fill()\"></i>";
+    favorite.className = "bi bi-heart";
+    favorite.onclick = "fill()";
 }
