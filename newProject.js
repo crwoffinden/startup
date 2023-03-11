14f4ae34017ca733f0296cca6bbb1cf8a476b5cc
crwoffinden <crwoffinden@gmail.com>
2023-03-10 22:33:19 -0700

    modified:   newProject.html 	modified:   newProject.js

--- newProject.js
+++ newProject.js
@@ -13,3 +13,8 @@ function toProfile() {
 function toUpload() {
     window.location.href = "upload.html";
 }
+
+function addInstrument() {
+    const instruments = document.getElementById("instruments");
+    instruments.innerHTML = "<tr class=\"instrument\">\n<select name=\"instrumentSelect\" id=\"select\">\n<option>Instrument</option>\n<option>Piano</option>\n<option>Guitar</option>\n<option>Xylophone</option>\n<option>Violin</option>\n</select>\n<div class=\"instrument-visual\">PLACEHOLDER</div> <!--Fix CSS--></tr>";
+}
