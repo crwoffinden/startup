b8ed6debfa6d85809448738d90c84c4b160cb556
crwoffinden <crwoffinden@gmail.com>
2023-03-13 13:39:32 -0600

    modified:   upload.js

--- upload.js
+++ upload.js
@@ -1,4 +1,5 @@
 function upload() {
     const title = document.getElementById("song-title");
     localStorage.setItem("title", JSON.stringify(title));
+    window.location.href = "myProjects.html";
 }
