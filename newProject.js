518c8a0d7f277c24660b76f5334c0ce6527decfe
crwoffinden <crwoffinden@gmail.com>
2023-03-30 19:34:11 -0600

    modified:   newProject.js

--- newProject.js
+++ newProject.js
@@ -74,13 +74,15 @@ class block {
     }
 }
 
+async function mouseup() {
+    return new Date();
+}
+
 function pressAndHold(button) {
     let time = 0;
-    let start = new Date();
-    let end = 0;
+    const start = new Date();
+    const end = await(mouseup);
 
-    //addEventListener('mousedown', start = new Date());
-    addEventListener('mouseup', end = new Date());
     time = end - start;
     if (time > 10000) time = 10000;
     time = time / 1000.0;
