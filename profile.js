b6d8e6cc8a4ed75564d18f3e5520fad22b3baf0c
crwoffinden <crwoffinden@gmail.com>
2023-04-10 14:49:19 -0600

    modified:   profile.html 	modified:   profile.js

--- profile.js
+++ profile.js
@@ -3,3 +3,9 @@ function fill() {
     if (favorite.className === "bi bi-heart") favorite.className = "bi bi-heart-fill";
     else if (favorite.className === "bi bi-heart-fill") favorite.className = "bi bi-heart";
 }
+
+function edit(icon) {
+    details = document.getElementById('editable');
+    details.innerHTML = "<input type=\"text\" class=\"name\">Firstname Lastname</input><input type=\"text\"class=\"bio\">This is where you tell us about yourself.</input>";
+    icon.innerHTML = "<i class=\"bi bi-save\" onclick=\"save(this)\"></i>";
+}
