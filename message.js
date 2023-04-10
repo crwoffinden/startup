b4b22dad276eabde515b2877b05ce4b1273b1044
crwoffinden <crwoffinden@gmail.com>
2023-04-10 17:13:55 -0600

    modified:   message.js

--- message.js
+++ message.js
@@ -5,7 +5,7 @@ function postMessage() {
     let messagesText = localStorage.getItem('messages');
     if (messagesText) messages = JSON.parse(messagesText);
     const newMessage = { message: message, date: date };
-    messages.splice[0, 0, newMessage];
+    messages.unshift(newMessage);
     localStorage.setItem('messages', JSON.stringify(messages));
     window.location.href = "profile.html";
 }
