810411dab922a6a1418fc786c56cfd547a115368
crwoffinden <crwoffinden@gmail.com>
2023-04-10 17:10:22 -0600

    modified:   message.js

--- message.js
+++ message.js
@@ -1,8 +1,9 @@
 function postMessage() {
     const message = document.getElementById('message').value;
     const date = new Date().toLocaleDateString();
+    let messages = [];
     let messagesText = localStorage.getItem('messages');
-    let messages = JSON.parse(messagesText);
+    if (messagesText) messages = JSON.parse(messagesText);
     const newMessage = { message: message, date: date };
     messages.splice[0, 0, newMessage];
     localStorage.setItem('messages', JSON.stringify(messages));
